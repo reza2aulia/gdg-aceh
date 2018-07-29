7530d8e01cd59fa070face2adfe2d97b76fe699d
reza2aulia <rreza252@gmail.com>
2018-07-29 20:54:40 +0700

    versi1

---
+++ helloworld.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html  dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>GDG Aceh Okay</title>
+  </head>
+  <body>
+    <h1>WELCOME TO GDG ACEH</h1>
+    <img src="brosur.jpg" alt="GDG Aceh Cool">
+  </body>
+</html>
